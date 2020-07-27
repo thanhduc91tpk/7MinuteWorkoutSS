a347f9373852c663517bccc23bf96d488ca48901
admin <thanhduc91tpk@gmail.com>
2020-07-27 17:55:51 +0700

    add on 4

---
+++ app/src/main/java/com/thanhduc91tpk/a7minuteworkouts/SplashActivity.kt
@@ -0,0 +1,21 @@
+package com.thanhduc91tpk.a7minuteworkouts
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        val handler = Handler()
+        handler.postDelayed({
+            val intent = Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        },3000)
+
+    }
+}
